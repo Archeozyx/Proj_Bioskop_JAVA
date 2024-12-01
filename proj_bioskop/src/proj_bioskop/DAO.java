f2b3fb1a5a7892fbc43b976a7f7bcd5f59d5fe5f
Reynard RadityaC <118745830+Archeozyx@users.noreply.github.com>
2024-12-01 22:26:19 +0700

    Delete proj_bioskop directory

--- proj_bioskop/src/proj_bioskop/DAO.java
+++
@@ -1,7 +0,0 @@
-package proj_bioskop;
-
-public class DAO {
-    
-    
-    
-}
--- proj_bioskop/src/proj_bioskop/MainClass.java
+++
@@ -1,9 +0,0 @@
-package proj_bioskop;
-
-public class MainClass {
-
-    public static void main(String[] args) {
-        
-    }
-    
-}
